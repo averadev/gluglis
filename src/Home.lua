dbf6279d047bd557ef736d7c8b590f3abc797b5f
Alberto <avera@geekbucket.com.mx>
2015-12-10 12:52:39 -0500

    Avatar-Flip en el Home

--- src/Home.lua
+++ src/Home.lua
@@ -19,72 +19,204 @@ local scene = composer.newScene()
 -- Variables
 local isCard = false
 local direction = 0
-local grpCards = {}
-local elemCards = {}
+local avaL = {}
+local avaR = {}
+local detail = {}
+local borders = {}
+local idxA, countA
+local lblName, lblAge, lblInts
+
+local loadUsers = {
+    {id = 1, photo = "mariana.jpeg", name = 'Mariana Gomez',  edad = 24, desc = 'Amante de la musica',
+        city='Cancun, Quitana Roo MX', lang='Español, Ingles', couch=1, car=0, available==1},
+    {id = 2, photo = "marcos.jpg", name = 'Marcos Jimenez',   edad = 26, desc = 'Adoro los gatos y el café',
+        city='Cancun, Quitana Roo MX', lang='Español, Ingles', couch=0, car=1, available==1},
+    {id = 3, photo = "andrew.jpg", name = 'Andrew Patterson', edad = 24, desc = 'Aventurero y deportista extremo',
+        city='Cancun, Quitana Roo MX', lang='Español, Frances', couch=1, car=1, available==0},
+    {id = 4, photo = "janine.jpg", name = 'Janine Smith',     edad = 24, desc = 'Me encanta el olor a pasto mojado',
+        city='Cancun, Quitana Roo MX', lang='Ingles, Aleman', couch=0, car=0, available==0},
+    {id = 5, photo = "victoria.jpg", name = 'Victoria Beckham', edad = 24, desc = 'Hermosa y PetFriendly',
+        city='Cancun, Quitana Roo MX', lang='Español, Frances', couch=1, car=1, available==1}
+}
 
 ---------------------------------------------------------------------------------
 -- FUNCIONES
 ---------------------------------------------------------------------------------
+-- Creamos primera tanda de tarjetas
 function firstCards()
+    idxA = 1
+    countA = #loadUsers
     
-    buildCard()
+    for i = 1, countA, 1 do
+        buildCard(loadUsers[i])
+    end
+    
+    lblName.text = loadUsers[1].name
+    lblAge.text = loadUsers[1].edad .. " años"
+    lblInts.text = loadUsers[1].desc
+    
+    avaL[1].alpha = 1
+    avaR[1].alpha = 1
+    borders[4].alpha = 1
+    borders[5].alpha = 1
+    borders[6].alpha = 1
+end
+
+-- Muestra imagenes y mascaras
+function buildCard(item)
+    local idx = #avaL + 1
+    -- Left
+    avaL[idx] = display.newImage("img/tmp/"..item.photo)
+    local mask = graphics.newMask( "img/maskal.jpg" )
+    avaL[idx].alpha = 0
+    avaL[idx].anchorY = 0
+    avaL[idx]:setMask( mask )
+    avaL[idx]:translate(midW, 176)
+    profiles:insert(avaL[idx])
+    -- Right
+    avaR[idx] = display.newImage("img/tmp/"..item.photo)
+    local mask = graphics.newMask( "img/maskar.jpg" )
+    avaR[idx].alpha = 0
+    avaR[idx].anchorY = 0
+    avaR[idx]:setMask( mask )
+    avaR[idx]:translate(midW, 176)
+    profiles:insert(avaR[idx])
+end
+
+-- Asigna informacion del usuario actual
+function setInfo(idx)
+    lblName.text = loadUsers[idx].name
+    lblAge.text = loadUsers[idx].edad
+    lblInts.text = loadUsers[idx].desc
+    detail[1].text = loadUsers[idx].city
+    detail[2].text = loadUsers[idx].lang
     
 end
 
-function buildCard()
-    local idx = #grpCards + 1
-    elemCards[idx] = {}
-    grpCards[idx] = display.newGroup()
-    screen:insert(grpCards[idx])
-    
-    elemCards[idx].f = display.newRect( midW, 172, 558, 558 )
-    elemCards[idx].f.anchorY = 0
-    elemCards[idx].f:setFillColor( 0, 193/225, 1 )
-    grpCards[idx]:insert(elemCards[idx].f)
-    
-    elemCards[idx].a = display.newImage("img/tmp/hugo.jpg")
-    elemCards[idx].a.anchorY = 0
-    elemCards[idx].a:translate(midW, 176)
-    grpCards[idx]:insert( elemCards[idx].a )
-    
+-- Muestra borders de revista
+function setBorder()
+    -- Left
+    if idxA == 1 then
+        borders[1].alpha, borders[2].alpha, borders[3].alpha = 0, 0, 0
+    elseif idxA == 2 then
+        borders[1].alpha, borders[2].alpha, borders[3].alpha = 0, 0, 1
+    elseif idxA == 3 then
+        borders[1].alpha, borders[2].alpha, borders[3].alpha = 0, 1, 1
+    elseif idxA >= 4 then
+        borders[1].alpha, borders[2].alpha, borders[3].alpha = 1, 1, 1
+    end
+    -- Rigth
+    if idxA == #loadUsers then
+        borders[4].alpha, borders[5].alpha, borders[6].alpha = 0, 0, 0
+    elseif (idxA+1) == #loadUsers then
+        borders[4].alpha, borders[5].alpha, borders[6].alpha = 0, 0, 1
+    elseif (idxA+2) == #loadUsers then
+        borders[4].alpha, borders[5].alpha, borders[6].alpha = 0, 1, 1
+    elseif (idxA+3) <= #loadUsers then
+        borders[4].alpha, borders[5].alpha, borders[6].alpha = 1, 1, 1
+    end
 end
 
 -- Listener Touch Screen
 function touchScreen(event)
     if event.phase == "began" then
-        print("yStart-"..event.yStart)
-        print("xStart-"..event.xStart)
         if event.yStart > 140 and event.yStart < 820 then
             isCard = true
             direction = 0
         end
     elseif event.phase == "moved" and (isCard) then
         local x = (event.x - event.xStart)
-        print("x: "..x)
+        local xM = (x * 3)
         if direction == 0 then
-            if x < -10 then
+            if x < -10 and idxA < #loadUsers then
                 direction = 1
-            elseif x > 10 then
+                avaR[idxA+1]:toBack()
+                avaR[idxA+1].alpha = 1
+                avaR[idxA+1].width = 550
+            elseif x > 10 and idxA > 1 then
                 direction = -1
+                avaL[idxA-1]:toBack()
+                avaL[idxA-1].alpha = 1
+                avaL[idxA-1].width = 550
             end
-        elseif direction == 1 then
-            elemCards[1].aR.width = 550 + (x * 2)
-            elemCards[1].fR.width = 558 + (x * 2)
-            elemCards[1].bgR.width = 608 + (x * 2.2)
-        elseif direction == -1 then
-            elemCards[1].aL.width = 550 - (x * 2)
-            elemCards[1].fL.width = 558 - (x * 2)
-            elemCards[1].bgL.width = 608 - (x * 2.2)
+        elseif direction == 1 and x <= 0 and xM >= -1100 then
+            if xM > -550 then
+                if avaR[idxA].alpha == 0 then
+                    avaL[idxA+1].alpha = 0
+                    avaR[idxA].alpha = 1
+                    avaR[idxA].width = 0
+                end
+                -- Move current to left
+                avaR[idxA].width = 550 + xM
+            else
+                if avaL[idxA+1].alpha == 0 then
+                    avaR[idxA].alpha = 0
+                    avaL[idxA+1]:toFront()
+                    avaL[idxA+1].alpha = 1
+                    avaL[idxA+1].width = 0
+                end
+                -- Move new to left
+                avaL[idxA+1].width = (xM*-1)-550
+            end
+        elseif direction == -1 and x >= 0 then
+            if xM < 550 then
+                if avaL[idxA].alpha == 0 then
+                    avaR[idxA-1].alpha = 0
+                    avaL[idxA].alpha = 1
+                    avaL[idxA].width = 0
+                end
+                -- Move current to left
+                avaL[idxA].width = 550 - xM
+            elseif xM < 1100 then
+                if avaR[idxA-1].alpha == 0 then
+                    avaL[idxA].alpha = 0
+                    avaR[idxA-1]:toFront()
+                    avaR[idxA-1].alpha = 1
+                    avaR[idxA-1].width = 0
+                end
+                -- Move new to left
+                avaR[idxA-1].width = xM - 550
+            end
             
         end
     elseif event.phase == "ended" or event.phase == "cancelled" then
-        elemCards[1].aL.width = 550
-        elemCards[1].fL.width = 558
-        elemCards[1].bgL.width = 608
-        
-        elemCards[1].aR.width = 550
-        elemCards[1].fR.width = 558
-        elemCards[1].bgR.width = 608
+        local xM = ((event.x - event.xStart) * 3)
+        -- To Rigth
+        if direction == 1 and xM >= -550 then
+            avaR[idxA].alpha = 1
+            avaL[idxA+1].alpha = 0
+            transition.to( avaR[idxA], { width = 550, time = 200, onComplete=function()
+                avaR[idxA+1].alpha = 0
+            end})
+        elseif direction == 1 and xM < -550 then
+            avaR[idxA].alpha = 0
+            avaL[idxA+1].alpha = 1
+            setInfo(idxA+1)
+            transition.to( avaL[idxA+1], { width = 550, time = 200, onComplete=function()
+                avaL[idxA].alpha = 0
+                avaR[idxA].alpha = 0
+                idxA = idxA + 1
+                setBorder()
+            end})
+        end
+        -- To Left
+        if direction == -1 and xM <= 550 then
+            avaL[idxA].alpha = 1
+            avaR[idxA-1].alpha = 0
+            transition.to( avaL[idxA], { width = 550, time = 200, onComplete=function()
+                avaR[idxA-1].alpha = 0
+            end})
+        elseif direction == -1 and xM > 550 then
+            avaL[idxA].alpha = 0
+            avaR[idxA-1].alpha = 1
+            setInfo(idxA-1)
+            transition.to( avaR[idxA-1], { width = 550, time = 200, onComplete=function()
+                avaL[idxA].alpha = 0
+                avaR[idxA].alpha = 0
+                idxA = idxA - 1
+                setBorder()
+            end})
+        end
         
         isCard = false
         direction = 0
@@ -95,7 +227,7 @@ end
 ---------------------------------------------------------------------------------
 -- DEFAULT METHODS
 ---------------------------------------------------------------------------------
-
+-- Called immediately on create scene
 function scene:create( event )
 	screen = self.view
     screen.y = h
@@ -109,15 +241,37 @@ function scene:create( event )
     tools = Tools:new()
     tools:buildHeader()
     screen:insert(tools)
+    screen:insert(tools)
     
     -- Content profile
     local bgCard = display.newRoundedRect( midW, 150, intW - 160, 700, 10 )
     bgCard.anchorY = 0
     bgCard:setFillColor( 11/225, 163/225, 212/225 )
     screen:insert(bgCard)
+    
+    bgAvatar = display.newRect( midW, 172, 580, 558 )
+    bgAvatar.anchorY = 0
+    bgAvatar:setFillColor( 0, 193/225, 1 )
+    screen:insert(bgAvatar)
+    
+    optB = {
+        {x = midW-284, y = 178, c = .6}, {x = midW-280, y = 177, c = .75}, {x = midW-276, y = 176, c = .9}, 
+        {x = midW+284, y = 178, c = .6}, {x = midW+280, y = 177, c = .75}, {x = midW+276, y = 176, c = .9}
+    }
+    for i = 1, #optB, 1 do
+        borders[i] = display.newRect( optB[i].x, optB[i].y, 6, 550 )
+        borders[i].anchorY = 0
+        borders[i].alpha = 0
+        borders[i]:setFillColor( optB[i].c )
+        screen:insert(borders[i])
+    end
+    
+    profiles = display.newGroup()
+    screen:insert(profiles)
+    
     -- Personal data
-    local lblName = display.newText({
-        text = "Ricardo Rodriguez", 
+    lblName = display.newText({
+        text = "", 
         x = 420, y = 760,
         width = 600,
         font = native.systemFontBold,   
@@ -125,8 +279,8 @@ function scene:create( event )
     })
     lblName:setFillColor( 1 )
     screen:insert(lblName)
-    local lblAge= display.newText({
-        text = "24 Años", 
+    lblAge= display.newText({
+        text = "", 
         x = 420, y = 795,
         width = 600,
         font = native.systemFont, 
@@ -134,8 +288,8 @@ function scene:create( event )
     })
     lblAge:setFillColor( 1 )
     screen:insert(lblAge)
-    local lblInts = display.newText({
-        text = "Amante de la Musica", 
+    lblInts = display.newText({
+        text = "", 
         x = 420, y = 820,
         width = 600,
         font = native.systemFont, 
@@ -144,6 +298,67 @@ function scene:create( event )
     lblInts:setFillColor( 1 )
     screen:insert(lblInts)
     
+    -- Position
+    local posY = 870
+    
+    -- BG Component
+    local bgComp1 = display.newRoundedRect( midW, posY, intW - 160, 390, 10 )
+    bgComp1.anchorY = 0
+    bgComp1:setFillColor( .88 )
+    screen:insert(bgComp1)
+    local bgComp2 = display.newRoundedRect( midW, posY, intW - 164, 386, 10 )
+    bgComp2.anchorY = 0
+    bgComp2:setFillColor( 1 )
+    screen:insert(bgComp2)
+    
+    -- Title
+    local bgTitle = display.newRoundedRect( midW, posY, intW - 160, 70, 10 )
+    bgTitle.anchorY = 0
+    bgTitle:setFillColor( .93 )
+    screen:insert(bgTitle)
+    local bgTitleX = display.newRect( midW, posY+60, intW - 160, 10 )
+    bgTitleX.anchorY = 0
+    bgTitleX:setFillColor( .93 )
+    screen:insert(bgTitleX)
+    local lblTitle = display.newText({
+        text = "DETALLE:", 
+        x = 310, y = posY+35,
+        width = 400,
+        font = native.systemFontBold,   
+        fontSize = 25, align = "left"
+    })
+    lblTitle:setFillColor( 0 )
+    screen:insert(lblTitle)
+    
+    -- Options
+    posY = posY + 55
+    local opt = {
+        {icon = 'icoFilterCity', label= 'Cancun, Quintana Roo Mexico'}, 
+        {icon = 'icoFilterLanguage', label= 'Ingles, Español e Italiano'}, 
+        {icon = 'icoFilterCheck', label= 'Ofrece Alojamiento'}, 
+        {icon = 'icoFilterUnCheck', label= 'Cuenta con Vehiculo Propio'}, 
+        {icon = 'icoFilterCheckAvailble', label= 'Disponible'}} 
+    for i=1, #opt do
+        posY = posY + 60
+        
+        local ico
+        if opt[i].icon ~= '' then
+            print("img/"..opt[i].icon..".png" )
+            ico = display.newImage( screen, "img/"..opt[i].icon..".png" )
+            ico:translate( 115, posY - 3 )
+        end
+        detail[i] = display.newText({
+            text = opt[i].label, 
+            x = 350, y = posY,
+            width = 400,
+            font = native.systemFont,   
+            fontSize = 25, align = "left"
+        })
+        detail[i]:setFillColor( 0 )
+        screen:insert(detail[i])
+    end
+    
+    --[[
     -- Buton
     local bgBtn = display.newRoundedRect( midW, 870, intW - 160, 70, 10 )
     bgBtn.anchorY = 0
@@ -165,10 +380,12 @@ function scene:create( event )
     local circle3 = display.newRoundedRect( midW + 50, 905, 25, 25, 13 )
     circle3:setFillColor( 1 )
     screen:insert(circle3)
+    ]]--
     
     firstCards()
-    --o:addEventListener( "touch", touchScreen )
+    o:addEventListener( "touch", touchScreen )
 end	
+
 -- Called immediately after scene has moved onscreen:
 function scene:show( event )
 end
