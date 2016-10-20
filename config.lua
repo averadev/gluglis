ce195cc147007ccdd12a3aca3c3656f63dc32dd8
alfredoChi <conomia_alfredo@hotmail.com>
2016-10-20 17:34:40 -0500

    error

--- config.lua
+++ config.lua
@@ -5,16 +5,5 @@ application = {
 	content = {
 		width = display.pixelWidth / mediaRes,
         height = display.pixelHeight / mediaRes
-	},
-	notification = 
-    {
-        iphone =
-        {
-            types = { "badge", "sound", "alert" }
-        },
-		google =
-        {
-            projectNumber = "3982849975315"
-        },
-    }
+	}
 }
